e086576164f7e04ba08f6d214c2abcffdbd93d5e
diyasruwandi <145577336+diyasruwandi@users.noreply.github.com>
2026-06-09 11:09:31 +0700

    adjust Work.tsx

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,14 +1,20 @@
 import { BriefcaseBusiness } from "lucide-react";
 import { workData } from "../data/work";
 import WorkCard from "./WorkCard";
+import BtnDownload from "./ui/btndownload";
 
 const Work = () => {
   return (
     <section className="space-y-4 mb-10">
       <div className="border-b border-neutral-800 pb-4 mb-6">
-        <h2 className="flex items-center gap-2 text-2xl font-semibold text-white">
-          <BriefcaseBusiness size={24} /> Career
-        </h2>
+        <div className="flex justify-between items-center">
+          <h2 className="flex items-center gap-2 text-2xl font-semibold text-white">
+            <BriefcaseBusiness size={24} /> Career
+          </h2>
+          <div className="translate-y-[6px]">
+            <BtnDownload />
+          </div>
+        </div>
         <p className="text-neutral-400 text-sm mt-2">My Career History</p>
       </div>
 
